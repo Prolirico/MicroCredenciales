cad9dee3e048488ccafff57041380992271e922e
Axel <Prolirico1@gmail.com>
2025-06-13 11:54:22 -0600

    Empece a trabajar el home y descargue mas imagenes para Assets

--- login-app/src/Screens/Home.js
+++ login-app/src/Screens/Home.js
@@ -1,10 +1,29 @@
 import React from "react";
 import styles from "./Home.module.css";
+import LogoSEDEQ from "../Assets/Secretaria-de-educacion-Queretaro.png";
 
 function Home() {
   return (
-    <div>
-      <h2 className={styles.Titulo}>Bienvenido al site</h2>
+    <div className={styles.fondoHome}>
+      {/* Header */}
+      <div className={styles.Header}>
+        <div className={styles.contenedorHeader1}>
+          <div>
+            <h1>MicroCredenciales SEDEQ</h1>
+          </div>
+        </div>
+        <div className={styles.contenedorHeader2}>
+          <img src={LogoSEDEQ} alt="Logo" />
+        </div>
+      </div>
+      {/* Content */}
+      <div className={styles.contenidoHome}>
+        <h2>Inicio</h2>
+      </div>
+      {/* Footer */}
+      <div className={styles.contenidoFooter}>
+        <h2>Footer</h2>
+      </div>
     </div>
   );
 }
